33e22290c5af649b3c14fad78f1bf390b8c8f8d5
Miguel <m.lozahenrich@gmx.de>
2017-09-12 21:36:33 +0200

    Amina klasse

---
+++ TestProjekt/src/miguel/Amina.java
@@ -0,0 +1,21 @@
+package miguel;
+
+public class Amina {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(aminakoyim(3, 5));
+		
+	}
+
+	public Amina() {
+		// TODO Auto-generated constructor stub
+		
+		
+	
+	}
+	public static int aminakoyim(int amk, int amk2){
+		int amk3 = amk + amk2;
+		 return amk3;
+	 }
+}
