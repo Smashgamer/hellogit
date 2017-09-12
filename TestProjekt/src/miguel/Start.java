89250f97ca3be5675224ff0edd5770ad5557b909
Marvin <Marvin@Lenovo-PC>
2017-09-12 20:49:20 +0200

    shit

--- TestProjekt/src/miguel/Start.java
+++ TestProjekt/src/miguel/Start.java
@@ -6,7 +6,7 @@ public class Start {
 	public static void main(String[] args) {
 		
 		//a greeting
-		System.out.println("Hello git");
+		System.out.println("Hello git (Miguel ist k1 keck)");
 
 	
 	}
